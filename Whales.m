faff8b41d103a743b3febdfa478f1fb916e8ad7c
hnoslen <nelsonhe17@grinnell.edu>
2016-03-05 23:14:28 -0600

    problem 4 work

--- Whales.m
+++ Whales.m
@@ -44,6 +44,19 @@ x = 0:150000;
 y1 = f1(r(1),a(1),K(1),x);
 y2 = f2(r(2),a(2),K(2),x);
 
+xlim = @(r,k,a) (((r(1)*r(2))/K(2))-(a(1)*r(2)))/(((r(1)*r(2))/(K(1)*K(2)))-(a(1)*a(2)));
+ylim = @(r,k,a) (((r(1)*r(2))/K(1))-(a(2)*r(1)))/(((r(1)*r(2))/(K(1)*K(2)))-(a(1)*a(2)));
+xlimVal = xlim(r,K,a);
+ylimVal = ylim(r,K,a);
+fig = figure;
+hax = axes;
+hold on
+plot(y1)
+plot(y2)
+plot(ylimVal*ones(1,length(y1)), 'r')
+line([xlimVal xlimVal],get(hax,'YLim'),'Color',[1 0 0])
+
+
 % problem 7
 profitplace = [12000; 6000].*r';
 gradProfit = [24000*(r(1)/K(1)) (12000*a(1))+(6000*a(2)); (12000*a(1))+(6000*a(2)) 12000*(r(2)/K(2))];
@@ -58,7 +71,7 @@ popsToMaxProfit = gradProfit\profitplace
 % Problem 4 max-value
 zerXPos = @(y,r,k,a) (k(1)*(r(1) - sum(a)*y + ((k(1)*k(2)*sum(a)^2*y.^2 - 2*k(1)*k(2)*sum(a)*r(1)*y + k(1)*k(2)*r(1)^2 - 4*r(2)*r(1)*y.^2 + 4*k(2)*r(2)*r(1)*y)./(k(1)*k(2))).^(1/2)))./(2*r(1));
 zerXNeg = @(y,r,k,a) -(k(1)*(sum(a)*y - r(1) + ((k(1)*k(2)*sum(a)^2*y.^2 - 2*k(1)*k(2)*sum(a)*r(1)*y + k(1)*k(2)*r(1)^2 - 4*r(2)*r(1)*y.^2 + 4*k(2)*r(2)*r(1)*y)./(k(1)*k(2))).^(1/2)))./(2*r(1));
-zerX = @(y,r,k,a) [zerXPos(y,r,k,a) zerXNeg(y,r,k,a)];
+zerX = @(y,r,k,a) zerXPos(y,r,k,a);% zerXNeg(y,r,k,a)];
 
 % d(foundx)/dy = (k ((a^2 y)/(2 r)-a/2)+s (1-(2 y)/l))/sqrt((k l (a^2 y^2-2 a r y)-r (4 s y (y-l)-k l r))/(k l))-(a k)/(2 r)
 
@@ -67,4 +80,13 @@ zerX = @(y,r,k,a) [zerXPos(y,r,k,a) zerXNeg(y,r,k,a)];
 
 maxYPos = @(r,s,k,l,a) (l*(2*r*(r*s*(k*r + l*s - a*k*l)*(k*s + l*r - a*k*l))^(1/2) + 2*k*r*s^2 + 2*l*r^2*s - a*k*(r*s*(k*r + l*s - a*k*l)*(k*s + l*r - a*k*l))^(1/2) - a*k*l*r^2 - a*k^2*r*s + a^2*k^2*l*r - 2*a*k*l*r*s))/(a^3*k^2*l^2 - a^2*k^2*l*s - a^2*k*l^2*r - 4*a*k*l*r*s + 4*k*r*s^2 + 4*l*r^2*s);
 maxYNeg = @(r,s,k,l,a) -(l*(2*r*(r*s*(k*r + l*s - a*k*l)*(k*s + l*r - a*k*l))^(1/2) - 2*k*r*s^2 - 2*l*r^2*s - a*k*(r*s*(k*r + l*s - a*k*l)*(k*s + l*r - a*k*l))^(1/2) + a*k*l*r^2 + a*k^2*r*s - a^2*k^2*l*r + 2*a*k*l*r*s))/(a^3*k^2*l^2 - a^2*k^2*l*s - a^2*k*l^2*r - 4*a*k*l*r*s + 4*k*r*s^2 + 4*l*r^2*s);
-maxY = @(r,k,a) max([maxYPos(r(1),r(2),k(1),k(2),sum(a)) maxYNeg(r(1),r(2),k(1),k(2),sum(a))]);
+maxY = @(r,k,a) [maxYPos(r(1),r(2),k(1),k(2),sum(a)) maxYNeg(r(1),r(2),k(1),k(2),sum(a))];
+
+mY = maxY(r,K,a);
+mX = zerX(mY,r,K,a);
+
+
+xmax = @(r,K,a) (r(2)-(r(1)/(a(1)*K(1)*K(2))))/(a(2)+(r(1)/(a(1)*K(1)*K(2))));
+ymax = @(xmax,r,k,a) (r(1)/a(1))*(1-(x/k(1)));
+
+
